7cf82d4b42555c6eea6eebdec7ddc0e7f35ef9c9
rajjaiswal10 <jraj15532@gmail.com>
2025-04-08 19:43:13 +0530

    fixes stashed changes

--- frontend/assets/js/dashboard.js
+++ frontend/assets/js/dashboard.js
@@ -188,12 +188,8 @@ async function loadData() {
     }
     
 async function syncData() {
-<<<<<<< Updated upstream
-    showAlert("Syncing data...", "info", 20000);   
-=======
-    showAlert("Syncing data...", "info", 60000);   
->>>>>>> Stashed changes
 
+    showAlert("Syncing data...", "info", 60000);   
     fetch('../backend/api/fitbit_data_fetch.php')
     .then(response => response.json())
     .then(data => {
