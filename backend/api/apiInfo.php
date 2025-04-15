0cea8e44a5d21cdfa302af525c6751d86f88c81e
Abhiofficial72 <118298548+Abhiofficial72@users.noreply.github.com>
2025-04-15 11:01:53 +0530

    ip->domain

--- backend/api/apiInfo.php
+++ backend/api/apiInfo.php
@@ -1,6 +1,6 @@
 <?php
 $client_id = "23QBW3";
-$redirect_uri = urlencode("http://52.66.243.41/health-dashboard/backend/api/fitbit_callback.php"); 
+$redirect_uri = urlencode("https://healthtracker.live/health-dashboard/backend/api/fitbit_callback.php"); 
 $scope = "activity heartrate sleep weight nutrition profile";
 $client_secret = "d7154cfe6421643e1463360bbb4ac8f5"; 
 ?>
